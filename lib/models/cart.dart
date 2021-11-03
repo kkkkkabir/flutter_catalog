0f7472b195a4ea457c3bb72bd620af36dc27f9a3
Kabir Jain <kabirjain15@gmail.com>
2021-11-03 17:19:04 +0530

    day 20 completed

---
+++ lib/models/cart.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_catalog/models/catalog.dart';
+
+class CartModel {
+  late CatalogModel _catalog;
+
+  // ignore: unused_field
+  final List<int> _itemIds = [];
+
+  // ignore: unnecessary_getters_setters
+  CatalogModel get catalog {
+    return _catalog;
+  }
+
+  // ignore: unnecessary_getters_setters
+  set catalog(CatalogModel newCatalog) {
+    _catalog = newCatalog;
+  }
+
+  //Get items in the cart
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  //Get total price
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  //Add item
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  //Remove item
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
